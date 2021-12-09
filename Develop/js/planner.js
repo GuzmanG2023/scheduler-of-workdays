6bc9e4a44d77e845ae5dbbdacbf2f22afe83c66b
St4d3ntG4zm4n <bwonder91@live.com>
2021-12-09 16:14:13 -0700

    added times and colors

--- Develop/js/planner.js
+++ Develop/js/planner.js
@@ -1,2 +1,24 @@
 var hour = moment().hour();
 console.log(hour,"hour")
+
+function colorHours () {
+    var hour = moment().hour();
+    // if (hour != 12) {hour = hour%12}
+    hour = Math.abs(hour - 9);
+    var descriptions = $(".description");
+    for (let i = 0; i < 9; i++) {
+        if (i == hour) {
+            $(descriptions[i]).addClass("present");
+        }
+        else if (i > hour) {
+            $(descriptions[i]).addClass("future");
+        }
+        else {
+            $(descriptions[i]).addClass("past");
+        }
+    }
+}
+
+setInterval(colorHours, 60000);
+
+colorHours();
